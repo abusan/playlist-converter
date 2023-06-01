1e9150168aaa2cdee20e949aca5bab070f1b5e6b
abusan3225 <m.aburaya1014@gmail.com>
2023-06-01 12:05:01 +0900

    コメントの追加

--- config/converter.php
+++ config/converter.php
@@ -1,15 +1,17 @@
 <?php
 
+// コンバート用の設定を記述します
 // ディレクトリは末尾スラッシュ無し
 
 return [
     'poweramp' => [
-        'inputPath' => '/mnt/d/doc/music/Playlists/Poweramp/In',
-        'outputPath' => '/mnt/d/doc/music/Playlists/Poweramp/Out',
-        'extension' => 'm3u8',
+        'inputPath'  => '/mnt/d/music/Playlists/Poweramp/In',   // 入力ファイルのベースとなるパス
+        'outputPath' => '/mnt/d/music/Playlists/Poweramp/Out',  // 出力ファイルのベースとなるパス
+        'extension'  => 'm3u8', // 対象となる拡張子
         'replacePairs' => [
+            // str_replace で使用する置換パターン
             'patterns' => [
-                'D:\\doc\\music\\',
+                'D:\\music\\',
                 '\\',
             ],
             'replacements' => [
